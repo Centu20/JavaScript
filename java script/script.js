448375b0c63537c7cd823ae1bf4f78ab5e418742
Centu20 <164720847+Centu20@users.noreply.github.com>
2024-05-11 14:24:10 -0600

    Add files via upload

--- java script/script.js
+++ java script/script.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+console.log("Hello world! :D");
+
+
+
+function prueba() {
+    console.log("prueba de la funcion");
+    let textInput = document.querySelector("#analisis").value;
+    let tituloH1 = document.querySelector("#beta");
+    console.log(textInput);
+    tituloH1.textContent = textInput;
+    tituloH1.style.color = 'rgb($(math.random() * 256),$(math.random() * 256),$(math.random() * 256)';
+
+}
