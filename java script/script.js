91d8a398c6d315b57f727caaa1b53cdc933bbe2c
Centu20 <164720847+Centu20@users.noreply.github.com>
2024-05-13 09:07:29 -0600

    Add files via upload

--- java script/script.js
+++ java script/script.js
@@ -6,8 +6,12 @@ function prueba() {
     console.log("prueba de la funcion");
     let textInput = document.querySelector("#analisis").value;
     let tituloH1 = document.querySelector("#beta");
+    let EstilosBody = document.querySelector("#cuerpo");
+    let FormaDiv = document.querySelector("#elemento");
     console.log(textInput);
     tituloH1.textContent = textInput;
-    tituloH1.style.color = 'rgb($(math.random() * 256),$(math.random() * 256),$(math.random() * 256)';
+    tituloH1.style.color = `rgb(${Math.random() * 256},${Math.random() * 256},${Math.random() * 256}`;
+    EstilosBody.style.backgroundColor = `rgb(${Math.random() * 256},${Math.random() * 256},${Math.random() * 256}`;
+    FormaDiv.style.backgroundColor = `rgb(${Math.random() * 256},${Math.random() * 256},${Math.random() * 256}`;
 
 }
